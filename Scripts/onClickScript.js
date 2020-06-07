2434fdb9b5e78d1db1b78bed274b19eab56baea9
chrisr98 <craymond098@gmail.com>
2020-06-06 23:17:53 -0400

    contact page needs UI revisions

--- Scripts/onClickScript.js
+++ Scripts/onClickScript.js
@@ -12,3 +12,21 @@ function show_dropdown() {
         dropdown.style.display = "none";
     }
 }
+
+function charLimit(limitField, limitNum) {
+    if (limitField.value.length > limitNum) {
+        limitField.value = limitField.value.substring(0, limitNum);}
+}
+
+function sendEmail() {
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const phone = document.getElementById("phone").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
+
+    let body = "Hey Chris-Emio, %0D%0A%0D%0A" + message + "%0D%0A%0D%0AFrom: "+ name + "%0D%0A%0D%0AEmail: " + email + "%0D%0A%0D%0APhone: " + phone;
+    let href = "mailto:chrisr98@bu.edu?subject="+subject+"&body="+body;
+    window.open(href);
+    window.location.href = "SentMail.html";
+}
