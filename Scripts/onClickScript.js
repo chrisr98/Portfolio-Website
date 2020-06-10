d589f9a0e378d1677f5da01b3f7d1a6a63a6e1a9
chrisr98 <craymond098@gmail.com>
2020-06-09 22:41:39 -0400

    portfolio page needs revision

--- Scripts/onClickScript.js
+++ Scripts/onClickScript.js
@@ -3,6 +3,8 @@ function show_dropdown() {
     menu_Bar.classList.toggle('active');
     const nav_Menu = document.getElementById('navigation');
     nav_Menu.classList.toggle('active');
+    const footer = document.getElementById('copyright');
+    footer.classList.toggle('active');
 
 
     const dropdown = document.getElementById('dropdown_content');
@@ -13,20 +15,17 @@ function show_dropdown() {
     }
 }
 
+
+//<textarea placeholder="Message" onkeyup="charLimit(this, 400);" required id="message"></textarea>
 function charLimit(limitField, limitNum) {
     if (limitField.value.length > limitNum) {
         limitField.value = limitField.value.substring(0, limitNum);}
 }
 
 function sendEmail() {
-    const name = document.getElementById("name").value;
-    const email = document.getElementById("email").value;
-    const phone = document.getElementById("phone").value;
-    const subject = document.getElementById("subject").value;
-    const message = document.getElementById("message").value;
+    let body = "Hey Chris-Emio, %0D%0A%0D%0A";
+    let href = "mailto:chrisr98@bu.edu?subject=&body="+body;
 
-    let body = "Hey Chris-Emio, %0D%0A%0D%0A" + message + "%0D%0A%0D%0AFrom: "+ name + "%0D%0A%0D%0AEmail: " + email + "%0D%0A%0D%0APhone: " + phone;
-    let href = "mailto:chrisr98@bu.edu?subject="+subject+"&body="+body;
-    window.open(href);
-    window.location.href = "SentMail.html";
+    window.location.href = href;
+
 }
